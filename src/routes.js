41902f7aac6cf006b9e4841f7904cccc6b982c18
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-07-06 14:19:33 -0300

    adicionado rota para upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
 const Router = require('express');
+const multer = require('multer');
+const multerConfig = require('./config/multer')
 
 const controller = require('./app/controllers/LegendaryController');
+const uploadFileController = require('./app/controllers/UploadFileController');
+
+const uploadFile = multer({ storage: multerConfig })
 
 const routes = new Router();
 
@@ -9,5 +14,6 @@ routes.get('/legendaries', controller.ListData);
 routes.post('/legendaries', controller.create);
 routes.put('/legendaries/:id', controller.update);
 routes.delete('/legendaries/:id', controller.delete);
+routes.post('/uploads', uploadFile.single('file'), uploadFileController.storeFile);
 
 module.exports = routes;
