dab4bf49d78d6fcbc908f510a78a24900c71ea5a
jaxolv <jaxolv@gmail.com>
2022-07-18 19:12:38 -0300

    primeiro commit

--- src/middlewares/TrainerValidator.js
+++ src/middlewares/TrainerValidator.js
@@ -29,7 +29,7 @@ async function TrainerValidator(request, response, next) {
   //   })
   // }
 
-  schema.validate({ name, age, city }).catch(err => {
+  await schema.validate({ name, age, city }).catch(err => {
     return response.status(400).json({
       message: err.errors
     })
