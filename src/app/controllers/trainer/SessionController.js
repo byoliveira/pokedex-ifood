90efb8d705a382539c6ffe139657ae0da56b41df
Bruno <bruno.yuri.alves.oliveira@outlook.com>
2022-07-22 19:50:05 -0300

    iniciando as implemntacoes parametros do jwt

--- src/app/controllers/trainer/SessionController.js
+++ src/app/controllers/trainer/SessionController.js
@@ -7,22 +7,28 @@ const SessionController = {
   create: (request, response) => {
     const { email, password } = request.body
 
-    const trainer = ListTrainerService.listAll()
+    const trainer = ListTrainerService.FindTrainer(email, password)
 
-    const trainerIndex = trainer.findIndex( t => t.email === email )
 
-    if(trainerIndex < 0) {
+  /*   if(trainerIndex < 0) {
       return response.status(401).json({error: 'Trainer not found'})
     }
 
     if(trainer[trainerIndex].password !== password) {
       return response.status(401).json({error: 'Invalid password'})
-    }
+    } */
 
     const {id, name} = trainer;
 
     return response.json({
-      
+      trainer: {
+        id,
+        name,
+        email
+      },
+      token: jwt.sign({ id }, 'a07bda8fd5e39462b4c3d860a36f6b4d', {
+        expiresIn: '5d'
+      })
     })
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const legendaryValidator = require('./middlewares/LegendaryValidator')
 const controller = require('./app/controllers/legendary/LegendaryController');
 const trainerController = require('./app/controllers/trainer/TrainerController')
 const uploadFileController = require('./app/controllers/utils/UploadFileController');
+const SessionController = require('./app/controllers/trainer/SessionController')
 
 const uploadFile = multer({ storage: multerConfig })
 
@@ -22,5 +23,7 @@ routes.get('/trainers', trainerController.listAll);
 routes.post('/trainers', trainerController.create);
 routes.put('/trainers/:id', trainerController.update);
 routes.post('/uploads', uploadFile.single('file'), uploadFileController.storeFile);
+routes.post('/session', SessionController.create);
+
 
 module.exports = routes;
