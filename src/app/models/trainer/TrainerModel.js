5cc0a9674549f2daf4514722bfdc9dbad8f179ad
Bruno <bruno.yuri.alves.oliveira@outlook.com>
2022-08-22 19:55:46 -0300

    configuracoes sequelize e iniciando model

--- src/app/models/trainer/TrainerModel.js
+++ src/app/models/trainer/TrainerModel.js
@@ -1,4 +1,24 @@
-function TrainerModel(id, name, email, password, age, city) {
+import Sequelize,{ Model } from 'sequelize'
+
+class TrainerModel extends Model {
+  static init(sequelize){
+    super.init({
+      id: Sequelize.UUIDV4(),
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      password: Sequelize.STRING,
+      age: Sequelize.STRING,
+      city: Sequelize.STRING
+    }, {
+      sequelize
+    })
+  }
+}
+
+export { TrainerModel }
+
+
+/* function TrainerModel(id, name, email, password, age, city) {
   this.id = id
   this.name = name
   this.email = email
@@ -7,4 +27,4 @@ function TrainerModel(id, name, email, password, age, city) {
   this.city = city
 }
 
-module.exports = TrainerModel
+module.exports = TrainerModel */
