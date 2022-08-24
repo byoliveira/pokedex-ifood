51ba9a704ecdf24bd4eff814e11855b6d6bf1544
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-08-24 20:46:30 -0300

    atualiza session controller para classe

--- src/app/controllers/auth/SessionController.js
+++ src/app/controllers/auth/SessionController.js
@@ -1,32 +1,31 @@
-const jwt = require('jsonwebtoken');
-const ListTrainerService = require('../../services/trainer/ListTrainerService');
-const controller = require('../../controllers/trainer/TrainerController');
+const jwt = require("jsonwebtoken");
+const ListTrainerService = require("../../services/trainer/ListTrainerService");
 
+class SessionController {
+  constructor() {}
 
-const SessionController = {
-  create: (request, response) => {
-    const { email, password } = request.body
+  static create(request, response) {
+    const { email, password } = request.body;
 
-    const trainer = ListTrainerService.FindTrainer(email, password)
+    const trainer = ListTrainerService.FindTrainer(email, password);
 
-
-  if(!trainer) {
-      return response.status(401).json({error: 'Trainer not found'})
+    if (!trainer) {
+      return response.status(401).json({ error: "Trainer not found" });
     }
 
-    const {id, name} = trainer;
+    const { id, name } = trainer;
 
     return response.json({
       trainer: {
         id,
         name,
-        email
+        email,
       },
-      token: jwt.sign({ id }, 'a07bda8fd5e39462b4c3d860a36f6b4d', {
-        expiresIn: '5d'
-      })
-    })
+      token: jwt.sign({ id }, "a07bda8fd5e39462b4c3d860a36f6b4d", {
+        expiresIn: "5d",
+      }),
+    });
   }
-}
+};
 
-module.exports = SessionController
+module.exports = SessionController;
